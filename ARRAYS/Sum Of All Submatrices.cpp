0de253c19c6bf45fe60bafd87ce62b3a9e86fa9a
I-MOHIT <mohitchugh180919@gmail.com>
2021-09-01 20:51:23 +0530

    Sum till right bottom - Sum till top left

--- ARRAYS/Sum Of All Submatrices.cpp
+++ ARRAYS/Sum Of All Submatrices.cpp
@@ -1,1 +1,17 @@
+// Given a 2D Matrix A of dimensions N*N, we need to return sum of all possible submatrices.
 
+// 1 <= N <=30
+
+// 0 <= A[i][j] <= 10
+
+int Solution::solve(vector<vector<int> > &A) {
+    int a=A.size();
+    int b=A[0].size();
+    int sum=0;
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            sum=sum+(i+1)*(j+1)*(a-i)*(b-j)*A[i][j];
+        }
+    }
+    return sum;
+}
