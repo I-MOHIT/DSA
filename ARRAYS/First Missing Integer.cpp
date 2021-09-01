b6a138ac7579dfa79d706cd914d8db8080ec3656
I-MOHIT <mohitchugh180919@gmail.com>
2021-09-01 20:47:52 +0530

    Use of A[A[i]] concept
    Checking the base case very carefully and using it to solve the further problem

--- ARRAYS/First Missing Integer.cpp
+++ ARRAYS/First Missing Integer.cpp
@@ -1,1 +1,33 @@
+// Given an unsorted integer array A of size N. Find the first missing positive integer.
+  
+// 1 <= N <= 1000000
 
+// -10^9 <= A[i] <= 10^9
+
+int Solution::firstMissingPositive(vector<int> &A) {
+    bool flag=false;
+    for(int i=0;i<A.size();i++){
+        if(A[i]==1){
+            flag=true;
+            break;
+        }
+    }
+    if(flag==false){
+        return 1;
+    }
+    int n=A.size();
+    for(int i=0;i<A.size();i++){
+        if(A[i]<=0||A[i]>n){
+            A[i]=1;
+        }
+    }
+    for(int i=0;i<n;i++){
+        A[(A[i]-1)%n]=A[(A[i]-1)%n]+n;
+    }
+    for(int i=0;i<n;i++){
+        if(A[i]<=n){
+            return i+1;
+        }
+    }
+    return n+1;
+}
