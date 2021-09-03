d3d727de87c62daac07a476f2ab8bb71c79d4d50
I-MOHIT <mohitchugh180919@gmail.com>
2021-09-04 01:28:19 +0530

    Update Wave Array.cpp

--- ARRAYS/Wave Array.cpp
+++ ARRAYS/Wave Array.cpp
@@ -1,1 +1,17 @@
+// Given an array of integers A, sort the array into a wave like array and return it, In other words, arrange the elements into a sequence such that
 
+// a1 >= a2 <= a3 >= a4 <= a5.....
+// If there are multiple answers possible, return the one that's lexicographically smallest.
+  
+// 1 <= len(A) <= 10^6
+// 1 <= A[i] <= 10^6
+
+vector<int> Solution::wave(vector<int> &A) {
+    sort(A.begin(),A.end());
+    for(int i=0;i<A.size()-1;i=i+2){
+        int temp=A[i];
+        A[i]=A[i+1];
+        A[i+1]=temp;
+    }
+    return A;
+}
